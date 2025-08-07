da937ca57fb464b6958e38d10a4a242a58a3e418
KIrillPrushak <kirillprushak@gmail.com>
2025-08-07 19:38:36 +0300

    change button color when switching

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import "./ControlPanel.scss";
+import { Locations } from "../../types/shops";
 
-function ControlPanel() {
+interface ControlPanelProps {
+  items: Locations;
+}
+
+function ControlPanel({ items }: ControlPanelProps) {
+  const [currentLocation, setCurrentLocation] = useState("minsk");
+  console.log(currentLocation);
+
   return (
     <section className="section-panel">
       <div className="title-panel">
         <h1>Магазины</h1>
       </div>
       <div className="control-panel">
-        <button>Минск</button>
-        <button>Слуцк</button>
-        <button>Солигорск</button>
+        {Object.keys(items).map((key) => {
+          const isActive = currentLocation === key;
+          return (
+            <button
+              className={`${isActive ? "isActive" : "unActive"}`}
+              key={key}
+              onClick={() => setCurrentLocation(key)}
+            >
+              {items[key].name}
+            </button>
+          );
+        })}
       </div>
     </section>
   );
