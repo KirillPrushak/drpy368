3b2e169a42575964c4388f6203a5b6189e6a0569
KIrillPrushak <kirillprushak@gmail.com>
2025-08-08 13:03:05 +0300

    add property to select

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,18 +5,20 @@ import "./Modal.scss";
 interface ModalProps {
   items: Locations;
   currentLocation: string;
+  data: any;
 }
 
-function Modal({ items, currentLocation }: ModalProps) {
+function Modal({ items, currentLocation, data }: ModalProps) {
   const currentLocationData = items[currentLocation];
   console.log(currentLocationData);
 
   return (
     <>
-      <TileLayer
+      {/* <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
       {currentLocationData.shops.map((shop) => {
         return (
           <Marker key={shop.id} position={shop.coordinates}>
@@ -28,7 +30,22 @@ function Modal({ items, currentLocation }: ModalProps) {
             </Popup>
           </Marker>
         );
-      })}
+      })} */}
+      {/* <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      /> */}
+      {/* {data.geometry.map()} */}
+      return (
+      <Marker position={[52.098306205095305, 23.760220530609157]}>
+        <Popup>
+          <div className="modal">
+            <h3>sdcsdvd</h3>
+            <p>Координаты: 1111</p>
+          </div>
+        </Popup>
+      </Marker>
+      ); z
     </>
   );
 }
--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer } from "react-leaflet";
+import { MapContainer, Marker, Polygon, Popup, TileLayer } from "react-leaflet";
 import "./Map.scss";
 import "leaflet/dist/leaflet.css";
 import { LatLngExpression } from "leaflet";
@@ -6,26 +6,87 @@ import { Locations } from "../../types/shops";
 import { locations } from "../../data/locations";
 import Modal from "../Modal/Modal";
 
+import L from "leaflet";
+import { dataCountry } from "../../data/dataCountry";
+
 interface MapCoordinatorProps {
   items: Locations;
   currentLocation: string;
 }
 
+interface PurpleOptions {
+  color: string;
+}
+
+const purpleOptions: PurpleOptions = { color: "purple" };
+
 function Map({ items, currentLocation }: MapCoordinatorProps) {
   const currentLocationData = locations[currentLocation];
 
   return (
-    <>
-      <MapContainer
-        center={currentLocationData.center}
-        zoom={11}
-        key={currentLocationData.name}
-        scrollWheelZoom={false}
-        className="map"
-      >
-        <Modal items={items} currentLocation={currentLocation} />
-      </MapContainer>
-    </>
+    <MapContainer
+      center={currentLocationData.center}
+      zoom={6}
+      key={currentLocationData.name}
+      scrollWheelZoom={true}
+      className="map"
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      {dataCountry.features.map((data: any) => {
+        const positions = data.geometry.coordinates[0].map((item: any) => [
+          item[1],
+          item[0],
+        ]);
+        const center = data.geometry.centerCoordinates;
+
+        const centerCoordinate = data?.position?.centerCoordinate;
+
+        return (
+          <>
+            <Polygon
+              pathOptions={purpleOptions}
+              positions={positions}
+              eventHandlers={{
+                mouseover: (e) => {
+                  const layer = e.target;
+                  console.log(layer);
+                  layer.setStyle({
+                    color: "#F88",
+                  });
+                },
+                mouseout: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    color: "#34dd50",
+                  });
+                },
+                click: (e) => {
+                  const layer = e.target;
+                  layer
+                    .bindPopup(
+                      `<div>
+                      <p>${data.properties.description}</p>
+                      <p><span>Координаты</span>: ${center}</p>
+                      <p>Население: ${data.properties.population}</p>
+                      </div>`
+                    )
+                    .openPopup();
+                },
+              }}
+            />
+            <Modal
+              items={items}
+              currentLocation={currentLocation}
+              data={data}
+            />
+          </>
+        );
+      })}
+    </MapContainer>
   );
 }
 
